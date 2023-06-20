ad0c181e05c2148d3215a248fa29daef9d732f0f
jishnulal7 <Jishnulal9497@gmail.com>
2023-06-20 22:19:23 +0530

    Added Restaurant Login Screen

---
+++ lib/screens/signup&in/restaurant_login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RestaurantLoginScreen extends StatelessWidget {
+  const RestaurantLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}

acfc8bf7c4afeef383a8bc20c1658deba02a3b61
jishnulal7 <Jishnulal9497@gmail.com>
2023-06-20 22:20:07 +0530

    Added Login Screen for Both Customer & Restaurant

---
+++ lib/screens/signup&in/main_login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainLoginScreen extends StatelessWidget {
+  const MainLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      resizeToAvoidBottomInset: false,
+      
+    );
+  }
+}
